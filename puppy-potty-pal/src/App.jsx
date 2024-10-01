65c25bd3b712ac6102accf048a61c4d8c0c823ad
Macca Williams <mackenzie050598@gmail.com>
2024-10-01 19:57:41 +0100

    more changes

--- puppy-potty-pal/src/App.jsx
+++ puppy-potty-pal/src/App.jsx
@@ -3,16 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import ButtonGradient from './assets/svg/ButtonGradient';
 import Button from './components/Button';
+import Header from './components/Header';
 const App = () => {
 
   return (
     <>
-    <h1>Welcome to puppy potty pal!</h1>
-
     <div className="pt-[4.75rem] lg:pt-[5.05rem] overflow-hidden">
-      <Button className="mt-10" href="#login">
-    Something
-      </Button>
+      <Header />
     </div>
     <ButtonGradient  />
 
